c13f87f155d0879888b4f02f43daa328aeec486d
cclaiborne <claiborne206@gmail.com>
2014-02-25 03:01:56 -0800

    clean up extraneous parens

--- wordinator.rb
+++ wordinator.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  json = JSON.generate [(params[:a].to_s.chars.sort == params[:b].to_s.chars.sort)]
+  json = JSON.generate [params[:a].to_s.chars.sort == params[:b].to_s.chars.sort]
 end
